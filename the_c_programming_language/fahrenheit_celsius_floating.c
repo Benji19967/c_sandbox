e86a5a7ab84e0b7492ca35c462f7606eb5541e8d
Benjamin Labrecque <labrecque.benji@gmail.com>
2024-03-21 15:52:53 +0100

    Extract constants using define

--- the_c_programming_language/fahrenheit_celsius_floating.c
+++ the_c_programming_language/fahrenheit_celsius_floating.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
+#define LOWER 0     // lower limit of table
+#define UPPER 300   // upper limit
+#define STEP  20    // step size 
+
 main() {
     /* print Fahrenheit-Celsius table 
      * for fahr = 0, 20, ... 300 */
 
     float fahr, celsius;
-    float lower, upper, step;
 
-    lower = 0;
-    upper = 300;
-    step = 20;
-
-    fahr = lower;
-    while (fahr <= upper) {
+    fahr = LOWER;
+    while (fahr <= UPPER) {
 
         // (5/9) would always yield 0 as integer division would be performed.
         //
@@ -24,7 +23,7 @@ main() {
         // `%6.1f`: print a floating point number 6 chars wide and use 1 decimal place.
         printf("%3.0f\t%6.1f\n", fahr, celsius);
 
-        fahr = fahr + step;
+        fahr = fahr + STEP;
     }
 }
 
