053e7a86da547869e0eba7bdd96cd80af83b1851
Benjamin Labrecque <labrecque.benji@gmail.com>
2024-03-22 17:01:07 +0100

    Add power function

---
+++ the_c_programming_language/power.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int power(int base, int exponent);
+
+main() {
+    for (int i = 0; i < 10; ++i) {
+        printf("%d %3d %6d\n", i, power(2, i), power(-3, i));
+    }
+}
+
+int power(int base, int exponent) {
+    int result = 1;
+
+    while (exponent > 0) {
+        result *= base;
+        --exponent;
+    }
+
+    return result;
+}
